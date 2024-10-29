57b614b15b21cf1861e67d34ec4cc2d6989b5911
schmitt777 <150674780+schmitt777@users.noreply.github.com>
2024-10-29 15:07:53 +0000

    .

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Educação Física',
+    'O que é Futebol?',
+    'Aquele jogo dos cara correndo atrás da bola e Neymar caido no chão'
 )
 
 criaCartao(
@@ -14,4 +14,14 @@ criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'esse menino nao sei nao',
+    'pq o kaique e assim?',
+    'ele é assim pq nao gosta de mulheres'
+)
+criaCartao(
+    'Fre fire',
+    'como dar capa no free fire',
+    'Aumentando a sensi e puxando a tela pra cima '
 )
